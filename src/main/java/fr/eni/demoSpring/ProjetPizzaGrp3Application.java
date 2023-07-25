7d106256d3ee320958042a050425b1b5fcdc49a0
Kévin <azerty.kevin@outlook.fr>
2023-07-25 16:51:55 +0200

    Création du projet

---
+++ src/main/java/fr/eni/demoSpring/ProjetPizzaGrp3Application.java
@@ -0,0 +1,13 @@
+package fr.eni.demoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPizzaGrp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetPizzaGrp3Application.class, args);
+	}
+
+}
