b11ff71303b38e70992f63c457abb78131958389
Kévin <azerty.kevin@outlook.fr>
2023-07-26 14:48:12 +0200

    correction erreur ArticleDAOJdbcImpl

--- src/main/java/fr/eni/projetPizza/dal/ArticleDAOJdbcImpl.java
+++ src/main/java/fr/eni/projetPizza/dal/ArticleDAOJdbcImpl.java
@@ -5,7 +5,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import fr.eni.projetPizza.bo.Article;
+import fr.eni.projetPizza.exception.DALException;
+
 public class ArticleDAOJdbcImpl implements ArticleDAO {
+
+	@Override
+	public void insert(Article article, int shoppingListId) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 }
