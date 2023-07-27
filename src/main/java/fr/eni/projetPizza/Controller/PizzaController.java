1cd4dd6e172415022e721a5434be95166ccc1818
Kévin <azerty.kevin@outlook.fr>
2023-07-27 12:19:00 +0200

    Création de fichier html/css

--- src/main/java/fr/eni/projetPizza/Controller/PizzaController.java
+++ src/main/java/fr/eni/projetPizza/Controller/PizzaController.java
@@ -1,19 +1,59 @@
-package fr.eni.projetPizza.Controller;
+package fr.eni.projetPizza.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PizzaController {
-    @GetMapping("/Acceuil")
+	
+	@GetMapping("/Connexion")
+    public String afficherConnexion() {
+        // Votre logique pour afficher la page "Connexion.html"
+        return "Connexion"; // renvoie à un fichier de vue nommé "Connexion.html"
+    }
+	
+    @GetMapping("/Accueil")
     public String afficherAcceuil() {
-        // Votre logique pour afficher les pizzas
-        return "Acceuil"; // renvoie à un fichier de vue nommé "Acceuil.html" par exemple
+        return "Accueil"; 
     }
 
+    @GetMapping("/Carte")
+    public String afficherCarte() {
+        return "Carte";
+    }
+    
+    @GetMapping("/Plats")
+    public String afficherPlats() {
+        return "Plats";
+    }
+    
+    @GetMapping("/Panier")
+    public String afficherPanier() {
+        return "Panier";
+    }
+    
+    @GetMapping("/Commande")
+    public String afficherCommande() {
+        return "Commande";
+    }
+    
     @GetMapping("/Table")
     public String afficherTable() {
-        // Votre logique pour afficher la page "Table.html"
-        return "Table"; // renvoie à un fichier de vue nommé "Table.html"
+        return "Table";
+    }
+    
+    @GetMapping("/TableDetail")
+    public String afficherTableDetail() {
+        return "TableDetail";
+    }
+    
+    @GetMapping("/Historique")
+    public String afficherHistorique() {
+        return "Historique";
+    }
+    
+    @GetMapping("/Preparation")
+    public String afficherPreparation() {
+        return "Preparation";
     }
 }
--- src/main/java/fr/eni/projetPizza/bll/ArticleManager.java
+++ src/main/java/fr/eni/projetPizza/bll/ArticleManager.java
@@ -1,12 +1,7 @@
 package fr.eni.projetPizza.bll;
 
-import com.microsoft.sqlserver.jdbc.StringUtils;
-
-import fr.eni.projetPizza.bo.Article;
 import fr.eni.projetPizza.dal.ArticleDAO;
 import fr.eni.projetPizza.dal.DAOFactory;
-import fr.eni.projetPizza.exception.BusinessException;
-import fr.eni.projetPizza.exception.DALException;
 
 public class ArticleManager {
 	private ArticleDAO articleDAO;
